22de90290d4af149b0e51084251c0955ce142df4
PabloSpr <sw.pablo@gmail.com>
2022-07-28 14:54:28 -0300

    cards

--- src/App.js
+++ src/App.js
@@ -20,25 +20,30 @@ function App() {
         <div className="row d-flex">
           {data.map((item) => (
             <div key={item.name} className="col-md-4">
-              {item.type === "bar" ? (
-                <BarChart
-                  chartData={item.data}
-                  options={options}
-                  title={item.title}
-                />
-              ) : item.type === "pie" ? (
-                <PieChart
-                  chartData={item.data}
-                  options={options}
-                  title={item.title}
-                />
-              ) : (
-                <LineChart
-                  chartData={item.data}
-                  options={options}
-                  title={item.title}
-                />
-              )}
+              <div className="card m-2">
+                <div className="card-header">{item.name}</div>
+                <div className="card-body">
+                  {item.type === "bar" ? (
+                    <BarChart
+                      chartData={item.data}
+                      options={options}
+                      title={item.title}
+                    />
+                  ) : item.type === "pie" ? (
+                    <PieChart
+                      chartData={item.data}
+                      options={options}
+                      title={item.title}
+                    />
+                  ) : (
+                    <LineChart
+                      chartData={item.data}
+                      options={options}
+                      title={item.title}
+                    />
+                  )}
+                </div>
+              </div>
             </div>
           ))}
         </div>
